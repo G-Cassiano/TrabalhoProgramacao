947e636a28ff75794912ec83bb9ba714236d7319
Gustavo Cassiano <gcolivei515@gmail.com>
2025-06-16 19:14:54 -0300

    Update Program.cs

--- 01 - Imobiliaria Retangular/Program.cs
+++ 01 - Imobiliaria Retangular/Program.cs
@@ -2,7 +2,7 @@
 Console.WriteLine("Digite a area em metros da frente do terreno: ");
 int AFrente = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("Digite a area em metros da lateral do terro: ");
+Console.WriteLine("Digite a area em metros da lateral do terreno: ");
 int ALado = Convert.ToInt32(Console.ReadLine());
 
 int ATotal = AFrente * ALado;
